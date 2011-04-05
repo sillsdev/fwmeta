3788b08f207c0ae7e2b04a8aad985840040002d0
Eberhard Beilharz <eb1@sil.org>
2011-04-05 15:19:34 +0200

    Deal with local files
    git calls editor for files that have full path name specified
    and also for files with relative path. Fixed script to deal
    with both.

--- winedit.sh
+++ winedit.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Use Wordpad as git editor
 file=${1//\//\\}
-# replace \c\ with c:\
-file=${file:1:1}:${file:2}
+# replace \c\ (or \d\ ...) with c:\ if filename starts with \
+[ "${file:0:1}" == "\\" ] && file=${file:1:1}:${file:2}
+echo "Editing $file"
 ${COMSPEC//\\//} /c "start /wait wordpad $file"
 
