180473de067bdb83c8749f3fd2ca2e39f75b8e4c
Neil Mayhew <neil_mayhew@sil.org>
2012-12-11 17:16:03 -0700

    Implement a create-mirror script
    This is useful for burning a set of repos to a DVD that can be sent to
    someone who is bandwidth-challenged,
    
    Change-Id: Iffc635e650a0bd29c7c8829c6518dd16e6d99933

---
+++ fwmeta/create-mirror.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Create a mirror of all the FW repos
+#
+# Useful for burning to DVD, or using as a local cache or proxy
+#
+# To use this script:
+#    mkdir mirror && cd mirror
+#    path/to/fwmeta/create-mirror.sh
+#
+# To use the mirror:
+#    git clone git://github.com/sillsdev/fwmeta.git fwrepo
+#    cd fwrepo
+#    git config --global url.file:///path/to/mirror/.insteadOf git://github.com/sillsdev/
+#    fwmeta/initrepo # Initial clone
+#    git config --global --unset url.file:///path/to/mirror/.insteadOf
+#    fwmeta/initrepo # Update to latest
+#
+# /path/to/mirror could be the location of your mounted DVD, and you would use
+# file:///media/gerrit.lsdev.sil.org/
+
+# Assume this script is located in fwmeta
+basedir=$(dirname "$(dirname "$0")")
+TOOLSDIR=fwmeta
+
+. "$basedir/$TOOLSDIR/functions.sh"
+
+for REPO in $(getAllRepos)
+do
+	echo "====> $REPO <===="
+
+	if [ ! -d "$REPO.git" ]
+	then
+		URL=$(getUrlForRepo "$REPO")
+		echo git clone --bare "$URL"
+		git clone --bare "$URL"
+	fi
+done
