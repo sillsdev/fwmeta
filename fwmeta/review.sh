ade76793eecc0b1aa0740f9499c4c6b185b63fa4
Eberhard Beilharz <eb1@sil.org>
2013-01-21 12:00:37 +0100

    Improve review script
    This makes it possible to upload changes on release and hotfix
    branches for review.
    
    Change-Id: I494d6f6954eb26d7e54f09118d751ab740c8be8f

--- fwmeta/review.sh
+++ fwmeta/review.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 # Submit a change for review
 
-if [ -z $1 ]; then
-	echo "ERROR: Please specify target branch"
-	exit 1
+. "$(dirname "$0")/functions.sh"
+
+pushQuiet()
+{
+	# used when running unit tests. Unfortunately passing -q to git push still produces some
+	# output on stderr, so we have to redirect that.
+	git push -q $@ 2>/dev/null
+}
+
+pushNormal()
+{
+	git push $@
+}
+
+pushCmd="pushNormal"
+if [ "$1" = "--quiet" ]; then
+	pushCmd="pushQuiet"
+	shift
 fi
 
-branch=$(git symbolic-ref -q HEAD || git name-rev --name-only HEAD 2>/dev/null)
-branch=${2:-${branch#refs/heads/}}
-git push origin HEAD:refs/for/$1/$branch
+feature=$(git config --get gitflow.prefix.feature || echo feature/)
+
+currentBranch=$(currentCommit)
+
+if [ -n "$1" ]; then
+	branch=$1
+	topic=$2
+else
+	branch=$(getParentBranch $currentBranch)
+	topic=${currentBranch#$feature}
+fi
+if [ -z "$branch" ]; then
+	branch=develop
+fi
+
+# prepend topic with / if set
+topic=${topic:+/$topic}
+# strip things like ~1
+topic="${topic%\~*}"
+
+$pushCmd origin HEAD:refs/for/$branch$topic
