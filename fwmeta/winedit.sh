1b9d3d8091c3f9f8e7c7060311f1124019b49ffe
Hasso Pape <hasso_pape@sil.org>
2016-05-09 14:44:28 -0500

    Use ZEdit as the default editor on Windows
    Replace WordPad, which is not unix-friendly on Windows 10
    
    Change-Id: Idf83eddd322532ccabb0ca8aa25880cc7a07e19d

--- fwmeta/winedit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Use Wordpad as git editor
-file=${1//\//\\}
-# replace \c\ (or \d\ ...) with c:\ if filename starts with \
-[ "${file:0:1}" == "\\" ] && file=${file:1:1}:${file:2}
-echo "Editing $file"
-${COMSPEC//\\//} /c "start /wait wordpad $file"
-
